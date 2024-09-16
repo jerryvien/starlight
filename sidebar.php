92f6462b5ec80345086294de6ab0fa2fcd509f28
jerryvien <jerryyee0902@gmail.com>
2024-09-16 18:00:54 +0800

    sidebar update

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,20 @@
 <!-- sidebar.php -->
-
+<!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <i class="fas fa-chart-line"></i> <!-- Corporate styled icon -->
         </div>
-        <div class="sidebar-brand-text mx-3">KEN Group Admin</div>
+        <div class="sidebar-brand-text mx-3">KEN Group</div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
+    <li class="nav-item active">
         <a class="nav-link" href="dashboard.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
@@ -24,69 +24,45 @@
     <hr class="sidebar-divider">
 
     <!-- Heading -->
-    <div class="sidebar-heading">Customer Management System</div>
+    <div class="sidebar-heading">
+        Interface
+    </div>
 
     <!-- Nav Item - Customer Management Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCustomer"
-            aria-expanded="true" aria-controls="collapseCustomer">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-users"></i>
-            <span>Customer</span>
+            <span>Customers</span>
         </a>
-        <div id="collapseCustomer" class="collapse" aria-labelledby="headingCustomer" data-parent="#accordionSidebar">
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Customer:</h6>
-                <a class="collapse-item" href="customer_register.php">
-                    <i class="fas fa-user-plus"></i> Create Customer
-                </a>
-                <a class="collapse-item" href="customer_listing.php">
-                    <i class="fas fa-address-book"></i> Customer Listing
-                </a>
+                <h6 class="collapse-header">Customer Operations:</h6>
+                <a class="collapse-item" href="customer_register.php">Create Customer</a>
+                <a class="collapse-item" href="customer_listing.php">Customer Listing</a>
             </div>
         </div>
     </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">Purchase Management System</div>
 
     <!-- Nav Item - Purchase Management Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePurchase"
-            aria-expanded="true" aria-controls="collapsePurchase">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-shopping-cart"></i>
-            <span>Purchase</span>
+            <span>Purchases</span>
         </a>
-        <div id="collapsePurchase" class="collapse" aria-labelledby="headingPurchase" data-parent="#accordionSidebar">
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Purchase Utilities:</h6>
-                <a class="collapse-item" href="purchase_entry.php">
-                    <i class="fas fa-shopping-cart"></i> New Purchase
-                </a>
-                <a class="collapse-item" href="purchase_listing.php">
-                    <i class="fas fa-list-alt"></i> Purchase Listing
-                </a>
-                <a class="collapse-item" href="purchase_report.php">
-                    <i class="fas fa-chart-line"></i> Report
-                </a>
+                <h6 class="collapse-header">Purchase Operations:</h6>
+                <a class="collapse-item" href="purchase_entry.php">New Purchase</a>
+                <a class="collapse-item" href="purchase_listing.php">Purchase Listing</a>
             </div>
         </div>
     </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
-
-    <!-- Nav Item - Logout -->
-    <li class="nav-item">
-        <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
-            <i class="fas fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
@@ -94,34 +70,4 @@
     </div>
 
 </ul>
-
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="logout.php">Logout</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<!-- Bootstrap core JavaScript-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="js/sb-admin-2.min.js"></script>
+<!-- End of Sidebar -->
