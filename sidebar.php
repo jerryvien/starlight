708d6958c428f8e288d4844d804884b1e9ae81a0
jerryvien <jerryyee0902@gmail.com>
2024-09-16 18:10:40 +0800

    blank

--- sidebar.php
+++ sidebar.php
@@ -60,6 +60,22 @@
             </div>
         </div>
     </li>
+    <!-- Nav Item - Purchase Management Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-shopping-cart"></i>
+            <span>Sample</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Purchase Operations:</h6>
+                <a class="collapse-item" href="blank.php">Blank</a>
+                <a class="collapse-item" href="purchase_listing.php">Purchase Listing</a>
+            </div>
+        </div>
+    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
