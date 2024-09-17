eaa81df27735836e3c96fc011f7bb74270d6c2a0
jerryvien <jerryyee0902@gmail.com>
2024-09-17 14:44:08 +0800

    asd

--- dashboard.php
+++ dashboard.php
@@ -210,10 +210,10 @@ try {
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Average Purchase Value</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$<?php echo number_format($sales_data['avg_order_value'], 2); ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">RM <?php echo number_format($sales_data['avg_order_value'], 2); ?></div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-bolt"></i>
+                                            <i class="fas fa-bolt text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
