4868225681b5dc4bc0d38171783f4b30436aacca
jerryvien <jerryyee0902@gmail.com>
2024-09-17 14:37:04 +0800

    asd

--- dashboard.php
+++ dashboard.php
@@ -194,7 +194,7 @@ try {
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Sales Per Day</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $sales_data['sales_per_day']; ?>Entry</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $sales_data['sales_per_day']; ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-chart-line fa-2x text-gray-300"></i>
@@ -203,9 +203,9 @@ try {
                                 </div>
                             </div>
                         </div>
-                        
+
                         <div class="col-xl-4 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
+                            <div class="card border-left-info shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
@@ -213,7 +213,7 @@ try {
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">$<?php echo number_format($sales_data['avg_order_value'], 2); ?></div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-chart-line fa-2x text-gray-300"></i>
+                                            <i class="fas fa-truck-fast fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
