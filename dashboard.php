67b784e5c28b957683b6d33a8a38c774fa4c4712
jerryvien <jerryyee0902@gmail.com>
2024-09-17 13:01:31 +0800

    asd

--- dashboard.php
+++ dashboard.php
@@ -154,7 +154,7 @@ if ($access_level === 'super_admin') {
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Sales</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$<?php echo number_format($total_sales['grand_total'], 2); ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$<?php echo number_format($average_order_value, 2); ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
