32d278abdbb066a54e83bbe207b95cae37f0ea04
jerryvien <jerryyee0902@gmail.com>
2024-10-01 14:22:24 +0800

    asd

--- winning_report.php
+++ winning_report.php
@@ -40,7 +40,7 @@ try {
     <title>Winning Records</title>
 
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
     <script src="vendor/chart.js/Chart.min.js"></script>
 
     <!-- Bootstrap CSS -->
