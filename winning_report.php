46ea7251173602581e6e198b9f0eeacc7303b9c2
jerryvien <jerryyee0902@gmail.com>
2024-10-02 14:09:35 +0800

    update table format winning table

--- winning_report.php
+++ winning_report.php
@@ -264,7 +264,7 @@ function generate_combinations($number) {
                                 <td><?php echo $record['winning_number']; ?></td>
                                 <td><?php echo $record['winning_game']; ?></td>
                                 
-                                <td><?php echo date('d/M/Y', strtotime($record['winning_date'])); ?></td>
+                                <td><?php echo date('d-M-Y', strtotime($record['winning_date'])); ?></td>
                                 <td>$$ <?php echo $record['winning_total_payout']; ?></td>
                                 <td>
                                 <form method="POST">
