866b7369447fb81b84eda5915b0665eec8f676dc
jerryvien <jerryyee0902@gmail.com>
2024-10-03 16:56:47 +0800

    asd

--- customer_analysis.php
+++ customer_analysis.php
@@ -223,11 +223,11 @@ $total_purchases = $win_count + $loss_count;
 
                 <!-- Combined Purchase and Win/Loss Records Table -->
                 <?php if (!empty($related_purchases)): ?>
-                <div class="card shadow mb-4 ml-4">
+                <div class="card shadow mb-4 ml-4 mr-4">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-primary">Purchase Entries and Win/Loss Records</h6>
                     </div>
-                    <div class="card-body mr-8">
+                    <div class="card-body">
                         <div class="table-responsive">
                             <table id="purchaseEntriesTable" class="table table-bordered" width="100%" cellspacing="0">
                                 <thead>
