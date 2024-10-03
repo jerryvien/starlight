d3b1115d0363ecafd93ebcc9b32a96b4348364bd
jerryvien <jerryyee0902@gmail.com>
2024-10-03 16:53:58 +0800

    asd

--- customer_analysis.php
+++ customer_analysis.php
@@ -124,6 +124,10 @@ $total_purchases = $win_count + $loss_count;
             <?php include('config/topbar.php'); ?>
 
             <div class="container-fluid">
+            <h1 class="h3 mb-2 text-gray-800">Customer Analysis Report</h1>
+                    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+                        For more information about DataTables, please visit the <a target="_blank"
+                            href="https://datatables.net">official DataTables documentation</a>.</p>
                 <div class="row">
                     <!-- Customer Data Table (left side, with ml-4) -->
                     <div class="col-md-5 ml-4">
