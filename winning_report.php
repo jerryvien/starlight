3b2ba5924ffc84afca966f859a0cc5f0cd054706
jerryvien <jerryyee0902@gmail.com>
2024-10-03 10:35:40 +0800

    asd

--- winning_report.php
+++ winning_report.php
@@ -218,9 +218,12 @@ function generate_combinations($number) {
     <title>Winning Record Matching</title>
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <script src="vendor/chart.js/Chart.min.js"></script>
+    
+    
+    <!--
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!--<link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
      Custom styles for this page -->
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
