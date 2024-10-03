b6fa29721ce8c6b452cfaebe5ccb385446f1bcda
jerryvien <jerryyee0902@gmail.com>
2024-10-03 17:53:52 +0800

    asdasd

--- config/sidebar.php
+++ config/sidebar.php
@@ -82,17 +82,14 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+    
 
     <!-- Nav Item - Customer Management Collapse Menu -->
         <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-address-card-o"></i>
-            <span>Customers</span>
+            <i class="fas fa-id-card"></i>
+            <span>Agents</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
@@ -103,6 +100,11 @@
             </div>
         </div>
     </li>
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 
 </ul>
 <!-- End of Sidebar -->
