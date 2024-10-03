104badf676585193aea121c8b17ccec81a005767
jerryvien <jerryyee0902@gmail.com>
2024-10-03 18:00:23 +0800

    asd

--- PHP_Data/top_winning_number.php
+++ PHP_Data/top_winning_number.php
@@ -53,7 +53,7 @@ foreach ($winning_numbers as $row) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top Winning Numbers</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    
 </head>
 <body>
 
