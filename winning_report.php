660b62b05980e4aeb35d5d60dd617664848745ab
jerryvien <jerryyee0902@gmail.com>
2024-10-04 16:48:00 +0800

    asd

--- winning_report.php
+++ winning_report.php
@@ -220,13 +220,13 @@ function generate_combinations($number) {
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     
     
-    <!--
+    <!--Custom styles for this page -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="vendor/chart.js/Chart.min.js"></script>
     <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-     Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+     
+    
 </head>
 <body>
 
