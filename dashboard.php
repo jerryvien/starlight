fba34713d8c13dc5028be2b3109b238c39fe5e3a
jerryvien <jerryyee0902@gmail.com>
2024-10-04 16:43:58 +0800

    asd

--- dashboard.php
+++ dashboard.php
@@ -461,6 +461,10 @@ try {
             <!-- End of Content Wrapper -->
         </div>
         <!-- End of Wrapper -->
+         <!-- Scroll to Top Button-->
+            <a class="scroll-to-top rounded" href="#page-top">
+                <i class="fas fa-angle-up"></i>
+            </a>
 
     <script>
     
