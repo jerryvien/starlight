a15d827e942fb609b3b5f69f09079890e98731c4
jerryvien <jerryyee0902@gmail.com>
2024-10-04 17:06:06 +0800

    asd

--- winning_report.php
+++ winning_report.php
@@ -223,7 +223,7 @@ function generate_combinations($number) {
     <!--Custom styles for this page -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="vendor/chart.js/Chart.min.js"></script>
-    <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+    
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
      
     
