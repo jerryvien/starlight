88c13db810e572ade5d2949b854afd022f662e44
jerryvien <jerryyee0902@gmail.com>
2024-10-04 20:46:22 +0800

    Change style

--- sales_report.php
+++ sales_report.php
@@ -80,7 +80,7 @@ $sales_by_month = $sales_by_month_stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sales Performance Dashboard</title>
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <style>
         .chart-container {
