937c430dec25eb978302bc6dc15672138395b786
jerryvien <jerryyee0902@gmail.com>
2024-10-04 16:40:47 +0800

    asd

--- purchase_listing.php
+++ purchase_listing.php
@@ -148,8 +148,7 @@ $bar_chart_data = json_encode(array_values($sales_by_customer));
     <!-- Custom styles for this template -->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    
 
 
 
