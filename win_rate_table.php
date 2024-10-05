44e214bf404cbbbb08409d4043dd007caea44bf6
jerryvien <jerryyee0902@gmail.com>
2024-10-05 10:48:08 +0800

    asd

--- win_rate_table.php
+++ win_rate_table.php
@@ -152,7 +152,7 @@ if ($total_transactions > 0) {
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Transaction Success Rate</div>
+                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Members WIN Rate</div>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo number_format($transaction_success_rate, 2); ?>%</div>
                                     </div>
                                     <div class="col-auto">
